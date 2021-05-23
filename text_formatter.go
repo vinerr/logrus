2c36e39d8e2ed0c0cd52b6866e37644549d79a8a
Penhauer Xiao <6280121+penhauer-xiao@users.noreply.github.com>
2021-05-23 14:27:44 +0800

    init

--- text_formatter.go
+++ text_formatter.go
@@ -301,14 +301,20 @@ func (f *TextFormatter) printColored(b *bytes.Buffer, entry *Entry, keys []strin
 	if msgLen > 1 {
 		if index, ok := exutf8.RuneIndexInString(entry.Message, 1); ok && index > 1 {
 			if overFloLen-2 >= msgCeilLen {
-				f.MsgReservedWidth = overFloLen - 2
 				if strings.Index(entry.Message, "债基") != -1 {
+					f.MsgReservedWidth = overFloLen - 4
 					fmt.Println("02:", msgLen, msgCeilLen, overLen, overFloLen, f.MsgReservedWidth, callerLen)
 				}
 				if strings.Index(entry.Message, "月还") != -1 {
+					f.MsgReservedWidth = overFloLen - 2
 					fmt.Println("02:", msgLen, msgCeilLen, overLen, overFloLen, f.MsgReservedWidth, callerLen)
 				}
-				if strings.Index(entry.Message, "还不够") != -1 {
+				if strings.Index(entry.Message, "为什么还不够呢") != -1 {
+					f.MsgReservedWidth = overFloLen - 7
+					fmt.Println("02:", msgLen, msgCeilLen, overLen, overFloLen, f.MsgReservedWidth, callerLen)
+				}
+				if strings.Index(entry.Message, "晕呵") != -1 {
+					f.MsgReservedWidth = overFloLen - 6
 					fmt.Println("02:", msgLen, msgCeilLen, overLen, overFloLen, f.MsgReservedWidth, callerLen)
 				}
 				// f.MsgReservedWidth = msgCeilLen
